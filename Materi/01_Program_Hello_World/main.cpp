ad18e6f0fe765af6a7e2ecbbf238460fa963433f
DedekAgung <dedekagungsetia12@gmail.com>
2022-12-14 03:26:57 +0700

    menyelesaikan materi hello world

---
+++ Materi/01_Program_Hello_World/main.cpp
@@ -0,0 +1,10 @@
+// 14-12-22
+// Memasukan perintah preprocessor C++
+#include <iostream> 
+// Agar dapat menggunakan fungsi, dll tanpa menggunakan perintah std (standard) didepannya.
+using namespace std;
+
+int main () {
+    cout << "Hello World" << endl;
+    return 0;
+}
